663a5718326b97ff541a90cdc79fbaf0502c6929
mihaibuica <mihaibuica38@gmail.com>
2020-04-08 17:35:14 +0300

    User added, home page default

--- ivizier/ivizier/urls.py
+++ ivizier/ivizier/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('avizier/', include('avizier.urls')),
+    path('', include('avizier.urls')),
 ]
