0375381dee670aaf74b66c7583a8f8375a8d1dcf
mihaibuica <mihaibuica38@gmail.com>
2020-04-08 19:27:19 +0300

    Page nav

--- ivizier/users/forms.py
+++ ivizier/users/forms.py
@@ -8,4 +8,4 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['username', 'email', 'password']
