ec8964c105a766df3f0c9af2e5009ec9b39cf2c7
Ioana-Ruxandra Stăncioi <32741332+Ruxandrra@users.noreply.github.com>
2020-05-13 17:11:23 +0300

    BD.1

--- ivizier/avizier/views.py
+++ ivizier/avizier/views.py
@@ -1,26 +1,64 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-# posts = [
-# 	{
-# 		'author': 'Ruxi Stancioi',
-# 		'title': 'Ruxi afis',
-# 		'content': 'primul proiect',
-# 		'date': '8 Aprilie'
-# 	},
-# 	{
-# 		'author': 'Angi',
-# 		'title': 'Angi gogosi',
-# 		'content': 'reteta',
-# 		'date': '10 Aprilie'
-# 	}
-# ]
+posts = [
+	{
+ 		'title': 'TârgulIT&C',
+ 		'image': 'avizier/images/targulitc.jpg',
+ 		'content': 'Peste 45 de companii de renume din România și din afară își vor face simțită prezența în cadrul evenimentului din acest an, oferind pasionaților de domeniul IT&C stagii de practică, internshipuri, joburi de junior și nu numai!',
+ 		'url': '/targulitc',
+ 		'date': '8 Aprilie'
+ 	},
+ 	{
+ 		'title': 'BESTEM-20',
+ 		'image': 'avizier/images/bestem.jpg',
+ 		'content': '1 day of teamwork, 24 hours to find innovative solutions for challenging problems, 1440 minutes of fun, delicious food and ingenuity, only at BEST Engineering Marathon aka BESTEM, a 24 hour-long hackathon at its 10th edition.',
+ 		'url': '/bestem',
+ 		'date': '8 Aprilie'
+ 	},
+ 	{
+ 		'title': 'HackITall',
+ 		'image': 'avizier/images/bestem.jpg',
+ 		'content': 'Ești pasionat de domeniul IT&C și vrei să surprinzi prin pasiunea și aptitudinile tale? A șaptea ediție a hackathon-ului organizat de LSAC București s-a lăsat așteptată, dar v-a pregatit 24 de ore memorabile în care creativitatea și skill-urile de programare vă vor ajuta sa faceți față provocării pregatite.',
+ 		'url': '/hackitall',
+ 		'date': '8 Aprilie'
+ 	},
+ 	{
+ 		'title': 'ELI-NP',
+ 		'image': 'avizier/images/eli-np.png',
+ 		'content': 'Va invitam sa vizitati platforma ELI-NP (laserul de la Magurele). Scopul vizitei este atat prezentarea platformei de cercetare, cat si atragerea studentilor spre potentiale proiecte de licenta/dizertatie, stagii de practica sau angajare. Vizitele vor avea loc in zilele de 3 si 4 Decembrie.',
+ 		'url': '/eli-np',
+ 		'date': '8 Aprilie'
+ 	},
+ 	{
+ 		'title': '3D_UPB',
+ 		'image': 'avizier/images/3dupb.png',
+ 		'content': 'Dragi studenti, Avem placerea sa va invitam la a 13-a editie a scolii de vara 3DPub, ce gazduieste, in perioada 15 Iunie - 7 August, ~15 workshop-uri in domeniile: - Game Development (programare & conceptie) - Grafica 3D (programare & modelare 3D) - Realitate virtuala si Realitate augmentata - GPGPU - 3D Computer vision (camere 3D, algoritmi)',
+ 		'url': '/3dupb',
+ 		'date': '8 Aprilie'
+ 	},
+ 	{
+ 		'title': 'Admitere ACS',
+ 		'image': 'avizier/images/acs.jpg"',
+ 		'content': 'Concursul de admitere pentru studiile de licență la Facultatea de Automatică și Calculatoare, începând din anul universitar 2020-2021, constă în două probe scrise: proba 1 - Algebră şi elemente de analiză matematică, proba 2 – la alegere între Fizică și Informatică.',
+ 		'url': '/acs',
+ 		'date': '8 Aprilie'
+ 	}
+ ]
+
 # Create your views here.
 def home(request):
 	context = {
-		'posts': Post.objects.all()
+		'posts': posts
 	}
 	return render(request, 'avizier/home.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'avizier/home.html'
+	context_object_name = 'posts'
+
 
 def targulitc(request):
 	return render(request, 'avizier/targulitc.html') # HttpResponse('<h1>Afis about</h1>')
